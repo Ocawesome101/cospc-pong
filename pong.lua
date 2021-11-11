b0d65f0af7b408076c58aa770ea1364c4029b54e
Ocawesome101 <ocawesome101@gmail.com>
2021-11-11 18:51:06 -0500

    padles, beginning of scoring system, input is a library

--- pong.lua
+++ pong.lua
@@ -4,7 +4,24 @@ local input = require("common.input")
 
 local w, h = term.getSize(2)
 
+local numbers = {
+  [0] = {
+  }, {
+  }, {
+  }, {
+  }, {
+  }, {
+  }, {
+  }, {
+  }, {
+  }, {
+  }
+}
+
 local ballX, ballY, ballDirX, ballDirY = 16, 16, 1, 1
+local ballSpeed = 2
+local paX, pbX = math.floor(w/2), math.floor(w/2)
+local paddleWidth, paddleHeight = 16, 8
 
 local function drawBall()
   term.drawPixels(ballX - 4, ballY - 4, 0x0, 8, 8)
@@ -18,22 +35,53 @@ local function drawCenterLine()
   end
 end
 
+local function drawPaddles()
+  term.drawPixels(paX - paddleWidth, 2, 0, paddleWidth*2, paddleHeight)
+  term.drawPixels(pbX - paddleWidth, h-2-paddleHeight, 0,
+    paddleWidth*2, paddleHeight)
+end
+
+local scoreA, scoreB = 0, 0
+
 term.setGraphicsMode(2)
-local pressed = {}
-local lastTimerID
+local is = input.new(1/40)
 while true do
+  term.setFrozen(true)
   term.clear()
   drawBall()
   drawCenterLine()
-  ballX = ballX + ballDirX
-  ballY = ballY + ballDirY
+  drawPaddles()
+  term.setFrozen(false)
+  ballX = ballX + ballDirX*ballSpeed
+  ballY = ballY + ballDirY*ballSpeed
   if ballX >= (w - 8) then
     ballDirX = -ballDirX
   elseif ballX <= 8 then
     ballDirX = -ballDirX
   end
-  if ballY >= (h - 8) or ballY <= 8 then
-    ballDirY = -ballDirY
+  if ballY >= (h - 8) then
+    if ballX >= pbX - paddleWidth and ballX <= pbX + paddleWidth then
+      ballDirY = -ballDirY
+    else
+      scoreA = scoreA + 1
+      ballX, ballY = math.floor(w/2), math.floor(h/2)
+    end
+  elseif ballY <= 8 then
+    if ballX >= paX - paddleWidth and ballX <= paX + paddleWidth then
+      ballDirY = -ballDirY
+    else
+      scoreB = scoreB + 1
+      ballX, ballY = math.floor(w/2), math.floor(h/2)
+    end
   end
-  
+  local sig = input.poll(is)
+  if sig[1] == "terminate" then
+    break
+  end
+  if is.pressed[keys.left] then
+    paX = math.max(paddleWidth, paX - 4)
+  end
+  if is.pressed[keys.right] then
+    paX = math.min(w - paddleWidth, paX + 4)
+  end
 end
